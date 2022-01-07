91be0a1a4bfe83fac2b92729372004876b4b0826
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2022-01-07 12:03:34 +0530

    Time: 606 ms (14.47%), Space: 30.3 MB (32.37%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,49 +1,43 @@
 class Solution {
 public:
+        bool check(int i,int j,vector<vector<bool>> &palin)
+        {
+                if(i<0 || j<0 || i>=palin.size() || j>=palin.size())
+                {
+                        return true;
+                }
+                return palin[i][j];
+       }
     string longestPalindrome(string s) {
-        int ans=0;
-            int max =-1;
-            int f=0;
-            for(int i=0;i<s.size();i++)
+     int n=s.length();
+            int a=0;
+            int b=0;
+            int max=0;
+            vector<vector<bool>> palin(n,vector<bool>(n,true));
+            for(int i=n-1;i>=0;i--)
             {
-                    for(int j=s.size()-1;j>=0;j--)
+                    for(int j=i;j<n;j++)
                     {
-                            if(s[i]==s[j])
-                               {
-                                   int a=i;
-                                    int b=j;
-                                    int flag=0;
-                                      while(a<=b)
-                                      {
-                                              if(s[a]==s[b])
-                                              {
-                                                      a++;
-                                                      b--;
-                                              }
-                                              else
-                                              {
-                                                      flag=1;
-                                                      break;
-                                              }
-                                                
-                                      }
-                                    if(flag==0 && (j-i+1)>max)
+                            if(s[i]==s[j] && check(i+1,j-1,palin)){
+                                    palin[i][j]=true;
+                                    if(j-i+1>max)
                                     {
-                                             max=j-i+1;
-                                            f=i;
+                                            max=j-i+1;
+                                            a=i;
+                                            b=j;
                                     }
-                               }
+                            }
+                            else
+                            {
+                                    palin[i][j]=false;
+                            }
                     }
-                    
             }
-            return s.substr(f,max);
-        
-            
-            
-            
-            
-            
-            
-            
-    }
+            string res="";
+            for(int i=a;i<=b;i++)
+            {
+                    res+=s[i];
+            }
+            return res;
+         }
 };
