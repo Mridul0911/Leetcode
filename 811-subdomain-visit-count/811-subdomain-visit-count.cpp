7b7366d2ce6d428f2d1dcac2ea9c2328886c37ed
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2022-01-08 10:26:10 +0530

    Time: 31 ms (5.80%), Space: 11.8 MB (43.48%) - LeetHub

---
+++ 811-subdomain-visit-count/811-subdomain-visit-count.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<string> subdomainVisits(vector<string>& cpdomains) {
+     vector<string> ans;
+            unordered_map<string,int> mp;
+            for(auto i:cpdomains)
+            {
+                    string s=i;
+                    string count="";
+                    int j=0;
+                    for(j=0;j<s.length();j++)
+                    {
+                            if(s[j]!=' ')
+                            {
+                                    count+=s[j];
+                            }
+                            else
+                            {
+                                    break;
+                            }
+                    }
+                    int cou=stoi(count);
+                    
+                    for (int k = j+1; k < s.size(); ++k)
+                     if (s[k] == '.')
+                        mp[s.substr(k+ 1)] += cou;
+         
+                    mp[s.substr(j+1)] += cou;
+                    
+            }
+            vector<string> res;
+        for (auto k : mp)
+            res.push_back (to_string(k.second) + " " + k.first);
+            
+            
+            return res;
+            
+            
+            
+    }
+};
