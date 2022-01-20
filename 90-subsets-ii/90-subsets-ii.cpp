3f55bd8049a54e6d9ff7e9bac677e8be21782723
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2022-01-20 10:56:20 +0530

    Time: 4 ms (70.21%), Space: 7.6 MB (72.99%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-         void sub(int index,vector<int> &nums,vector<int> &arr,set<vector<int>> &ans,int n)
+        /* void sub(int index,vector<int> &nums,vector<int> &arr,set<vector<int>> &ans,int n)
         {
                 if(index==n)
                 {
@@ -11,18 +11,29 @@ public:
                 sub(index+1,nums,arr,ans,n);
                 arr.pop_back();
                 sub(index+1,nums,arr,ans,n);
-        }
+        } */
+         void sub(int index,vector<int> &nums,vector<int> &arr,vector<vector<int>> &ans)
+        {
+                   ans.push_back(arr);
+                 for(int i=index;i<nums.size();i++)
+                 {
+                    if(i!=index && nums[i]==nums[i-1]) continue;
+                         arr.push_back(nums[i]);
+                         sub(i+1,nums,arr,ans);
+                         arr.pop_back();
+                 }
+              
+         
+         } 
+        
+        
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
          sort(nums.begin(),nums.end());
-            int n=nums.size();
-           set<vector<int>> ans;
+            
+           vector<vector<int>> ans;
             vector<int> inp;
-            sub(0,nums,inp,ans,n); 
-            vector<vector<int>> ans2;
-            for(auto it:ans)
-            {
-                    ans2.push_back(it);
-            }
-            return ans2;
+            sub(0,nums,inp,ans); 
+            
+            return ans;
    }
 };
