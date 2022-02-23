a08e251e6dc44ea8010456322cd9d90a5930c2cd
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2022-02-23 17:04:58 +0530

    Time: 222 ms (17.58%), Space: 7.4 MB (66.15%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.cpp
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
-      int find(vector<int>& nums,int i,int j)
-    {
-        if(i>j) return 0;
-        int left = nums[i]+min(find(nums,i+2,j),find(nums,i+1,j-1));
-        int right = nums[j]+min(find(nums,i,j-2),find(nums,i+1,j-1));
-        return max(left,right);
-    }
-     bool PredictTheWinner(vector<int>& nums) 
-     {
-          int n=nums.size();
-        int sum=0;
+        int find(vector<int> &nums,int i,int j)
+        {
+                if(i>j)
+                {
+                        return 0;
+                }
+                int left=nums[i]+min(find(nums,i+2,j),find(nums,i+1,j-1));
+                int right=nums[j]+min(find(nums,i+1,j-1),find(nums,i,j-2));
+                return max(left,right);
+        }
+    bool PredictTheWinner(vector<int>& nums) {
+         int i=0;
+            int j=nums.size()-1;
+            int total=0;
             for(auto i:nums)
             {
-                    sum+=i;
+                    total+=i;
             }
-        int score=find(nums,0,n-1);
-        if(score>=sum-score) return true;
-        else return false;
-     }
+            int player1=find(nums,i,j);
+           if(player1>=total-player1)
+           {
+                   return true;
+           }
+            else
+            {
+                    return false;
+            }
+            
+    }
 };
