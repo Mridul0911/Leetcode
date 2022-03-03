8dcbf5182e89ab290a53da58bb967a12caa2aefd
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2022-03-03 14:38:08 +0530

    Time: 0 ms (100.00%), Space: 9.9 MB (5.06%) - LeetHub

---
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+        int dp[5001][101];
+        int solve(vector<int> &nums,int start,int count)
+        {
+                if(start>=nums.size()-2)
+                {
+                        return 0;
+                }
+                if(dp[start][count]!=-1)
+                {
+                        return dp[start][count];
+                }
+                if(nums[start+1]-nums[start]==nums[start+2]-nums[start+1])
+                {
+                        count++;
+                        return dp[start][count]=count+solve(nums,start+1,count);
+                }
+                else
+                {
+                        return dp[start][count]=solve(nums,start+1,0);
+                }
+        }
+        
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        
+            int n=nums.size();
+            memset(dp,-1,sizeof dp);
+            if(n>=3)
+            {
+                    cout<<"HELLO";
+                    return solve(nums,0,0);
+            }
+            else
+            {
+                    return 0;
+            }
+   }
+};
