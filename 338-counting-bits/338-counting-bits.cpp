5338d40c4916e12ac7b2e12cc8ef1522b65d1be3
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2022-03-03 17:32:59 +0530

    Time: 11 ms (30.77%), Space: 8.5 MB (34.56%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+        int findbits(int i)
+        {
+                int count=0;
+                while(i!=0)
+                {
+                        if(i%2==1)
+                        {
+                            count++;    
+                        }
+                        i=i/2;
+                }
+                return count;
+        }
+    vector<int> countBits(int n) {
+        vector<int> ans;
+            for(int i=0;i<=n;i++)
+            {
+                   
+                    ans.push_back(findbits(i));
+            }
+            return ans;
+            
+            
+    }
+};
