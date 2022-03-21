6ea8277ed6c4e83fe394e6758971d864fb0db860
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2022-03-21 17:55:22 +0530

    Time: 4 ms (72.57%), Space: 7.3 MB (5.91%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -1,5 +1,31 @@
 class Solution {
 public:
+         // vector<int> freq(26,-1);
+         //    for(int i=0;i<s.size();i++)
+         //    {
+         //            freq[s[i]-'a']=i;
+         //    }
+         //    // for(auto i:freq)
+         //    // {
+         //    //         cout<<i<<endl;
+         //    // }
+         //   vector<int> ans;
+         //    int j=0;
+         //    int curr=0;
+         //    for(int i=0;i<s.size();i++)
+         //    {
+         //            j=max(j,freq[s[i]-'a']);
+         //            if(i==j)
+         //            {
+         //                    ans.push_back(i-curr);
+         //                    curr=i+1;
+         //            }
+         //    }
+         //    for(int i=0;i<ans.size();i++)
+         //    {
+         //            ans[i]=ans[i]+1;
+         //    }
+         //        return ans;    
     vector<int> partitionLabels(string S) {
         vector<vector<int>> intervals(26, vector<int>(2,-1));
         vector<int> res;
@@ -9,22 +35,24 @@ public:
         }
             
               vector<vector<int>> ans;
-       
-                        sort(intervals.begin(),intervals.end());
+            sort(intervals.begin(),intervals.end());
           
             vector<int> th=intervals[0];
             
-            for(auto it:intervals){
-               if(th[1]>=it[0]){
+            for(auto it:intervals)
+            {
+               if(th[1]>=it[0])
+               {
                     th[1]=max(th[1],it[1]);   
                }
-                    else{
+                    else
+                    {
                             ans.push_back(th);
                             th=it;
                     }
-           }
-                            ans.push_back(th);
-            string gg="";
+           } 
+            ans.push_back(th);
+            
             for(auto i:ans)
             {
                     if(i[0]==-1 || i[1]==-1) continue;
@@ -33,10 +61,5 @@ public:
                     
             }
             return res;
-            
-            
-            
-            
-            
-    }
+     }
 };
