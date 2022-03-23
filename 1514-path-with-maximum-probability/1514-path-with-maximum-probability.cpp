caaf3367b2e2c2c8bac1efde3c2ffe1674d1c010
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2022-03-23 12:22:07 +0530

    Time: 750 ms (5.05%), Space: 74.4 MB (9.13%) - LeetHub

---
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) 
+    {
+        vector<vector<pair<int,double>>> adj(n);
+            int i=0;
+            for(auto gg:edges)
+            {
+                    adj[gg[0]].push_back({gg[1],succProb[i]});
+                    adj[gg[1]].push_back({gg[0],succProb[i]});
+               i++;            
+            }
+            
+            
+            vector<double> dis(n,0);
+            priority_queue<pair<int,double>,vector<pair<int,double>>> pq;
+            pq.push({1,start});
+            dis[start]=1;
+            
+            while(!pq.empty())
+            {
+                    int gg=pq.top().second;
+                    pq.pop();
+                    for(auto hh:adj[gg])
+                    {
+                          int v=hh.first;
+                            double weight=hh.second;
+                            if(dis[v]<dis[gg]*weight)
+                            {
+                                    dis[v]=dis[gg]*weight;
+                                    pq.push({dis[v],v});
+                            }
+                    }
+            }
+            return dis[end];
+    }
+};
