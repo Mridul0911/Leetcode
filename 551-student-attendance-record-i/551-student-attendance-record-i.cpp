858567e88128b6da14bef1b1f3d13f30fb272bff
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2022-03-23 07:43:25 +0530

    Time: 2 ms (46.04%), Space: 6.1 MB (79.69%) - LeetHub

---
+++ 551-student-attendance-record-i/551-student-attendance-record-i.cpp
@@ -0,0 +1,34 @@
+class Solution 
+{
+public:
+    bool checkRecord(string s) 
+    {
+      int a=0;
+            int l=0;
+            for(int i=0;i<s.size();i++)
+            {
+                    if(s[i]=='A')
+                    {
+                            a++;
+                    }
+                    if(s[i]=='L')
+                    {
+                            l++;
+                    }
+                    else
+                    {
+                            l=0;
+                    }
+                    if(a>=2)
+                    {
+                            return false;
+                    }
+                    if(l>=3)
+                    {
+                            return false;
+                    }
+            }
+            return true;
+            
+    }
+};
