75c6e3c663cc41a5a4d1d6a5eba3f425950fa387
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2022-04-05 15:11:30 +0530

    Time: 0 ms (100.00%), Space: 6 MB (17.06%) - LeetHub

---
+++ 367-valid-perfect-square/367-valid-perfect-square.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+    if (num >= 0) 
+    {
+     long long sr = sqrt(num);
+        return (sr * sr == num);
+    }
+    
+    return false;
+   }
+};
