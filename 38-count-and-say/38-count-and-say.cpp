592d57068a3c575b599d35390d5ca1e8edd71b71
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2022-04-14 16:14:00 +0530

    Time: 12 ms (48.45%), Space: 6.3 MB (99.16%) - LeetHub

---
+++ 38-count-and-say/38-count-and-say.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+        
+    string countAndSay(int n) {
+        string ans="1";
+            unordered_map<char,int> ump;
+            string temp;
+            string finalans="";
+            for(int i=2;i<=n;i++)
+            {
+                    temp="";
+                  for(int i=0;i<ans.size();i++)
+                  {
+                          temp+=ans[i];
+                  }
+                    ans="";
+                  for(int j=0;j<temp.size();j++)
+                   {
+                      ump[temp[j]] += 1;
+                        if(j<temp.size() && temp[j]==temp[j+1])
+                                continue;
+                        else
+                                ans += to_string(ump[temp[j]]) + temp[j];
+                     ump[temp[j]]=0;
+                    }
+                    ump.clear();
+             }
+            return ans;
+            
+            
+    }
+};
