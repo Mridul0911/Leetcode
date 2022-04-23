eddd2e2430f60b24228f3ed0d7e96840209ae709
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2022-04-23 11:30:26 +0530

    Time: 3 ms (83.88%), Space: 6.7 MB (74.39%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -3,11 +3,15 @@ public:
 
     // Encodes a URL to a shortened URL.
     string encode(string longUrl) {
+            ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
      return longUrl;   
     }
 
     // Decodes a shortened URL to its original URL.
     string decode(string shortUrl) {
+            ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
         return shortUrl;
     }
 };
