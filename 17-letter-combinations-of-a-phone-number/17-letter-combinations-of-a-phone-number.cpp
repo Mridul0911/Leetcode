abc70a78242f098d061a477d3b096e0dc3244ec0
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2022-05-09 23:31:45 +0530

    Time: 2 ms (44.02%), Space: 6.6 MB (60.00%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,39 +1,41 @@
 class Solution {
 public:
-        void solve(string &digits,map<char,string> &mp,string &s,vector<string> &ans,int index)
-        {
-                if(index==digits.length())
-                {
-                        ans.push_back(s);
-                        return;
-                }
-                char currNumber=digits[index];
-                string str=mp[currNumber];
-                for(int i=0;i<str.size();i++)
-                {
-                        s+=str[i];
-                        solve(digits,mp,s,ans,index+1);
-                        s.pop_back();
-                }
-        }
-    vector<string> letterCombinations(string digits) {
-        map<char,string> mp{
-                {'2',"abc"},
-                {'3',"def"},
-                {'4',"ghi"},
-                {'5',"jkl"},
-                {'6',"mno"},
-                {'7',"pqrs"},
-                {'8',"tuv"},
-                {'9',"wxyz"},
-             };
-            string s="";
+       void solve(string digits,map<char,string> &mp,vector<string> &ans,string &s,int index)
+       {
+               if(index==digits.length())
+               {
+                       ans.push_back(s);
+                       return;
+               }
+               char number=digits[index];
+               string t=mp[number];
+               for(int i=0;i<t.length();i++)
+               {
+                       s+=t[i];
+                       solve(digits,mp,ans,s,index+1);
+                       s.pop_back();
+               }
+       }
+    vector<string> letterCombinations(string digits) 
+    {
+       map<char,string> mp={{'2',"abc"},
+                            {'3',"def"},
+                            {'4',"ghi"},
+                            {'5',"jkl"},
+                            {'6',"mno"},
+                            {'7',"pqrs"},
+                            {'8',"tuv"},
+                            {'9',"wxyz"}
+                        };
             vector<string> ans;
             if(digits.length()==0)
             {
                     return ans;
             }
-            solve(digits,mp,s,ans,0);
-            return ans;
+            string s="";
+            solve(digits,mp,ans,s,0);
+    return ans;       
+            
+            
     }
 };
