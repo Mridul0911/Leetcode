b2662fab0b8a94ae450b2f90201e6c81529be71a
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2022-05-10 15:55:04 +0530

    Time: 3 ms (45.41%), Space: 6.4 MB (58.80%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -11,19 +11,18 @@ public:
                 }
                 for(int i=index;i<nums.size();i++)
                 {
-                        if(i>index && nums[i]==nums[i-1]) continue;
-                        if(nums[i]>target)
-                        {
-                                break;
-                        }
+                      //  if(i>index && nums[i]==nums[i-1]) continue;
+                       // if(nums[i]>target)
+                        //{
+                   //             break;
+                       // }
                         ds.push_back(nums[i]);
                         subsq(i+1,ans,ds,nums,k,target-nums[i]);
                         ds.pop_back();
                 }
-                
         }
-    vector<vector<int>> combinationSum3(int k, int n) {
-        
+    vector<vector<int>> combinationSum3(int k, int n) 
+    {
             vector<int> nums;
             for(int i=1;i<=9;i++)
             {
@@ -33,8 +32,5 @@ public:
             vector<int> inp;
             subsq(0,ans,inp,nums,k,n);
             return ans;
-            
-            
-            
     }
 };
