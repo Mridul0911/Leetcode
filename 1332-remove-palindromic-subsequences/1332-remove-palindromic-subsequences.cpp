a273a4734c6f68c5bec2b32a9851982c32125ea7
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2022-06-08 12:01:43 +0530

    Time: 6 ms (7.40%), Space: 6.2 MB (78.83%) - LeetHub

---
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        string g=s;
+            reverse(g.begin(),g.end());
+            if(s==g)
+            {
+                    return 1;
+            }
+            else
+            {
+                    return 2;
+            }
+            
+            
+    }
+};
