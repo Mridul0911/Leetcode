7cea5b4eb68394c17bdd514c82655d2a83b1189e
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2022-06-28 12:31:04 +0530

    Time: 1090 ms (17.38%), Space: 207.3 MB (20.16%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -22,6 +22,12 @@ public:
             int n=prices.size();
             int cap=0;
             vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+            
+            vector<vector<int>> ahead(2,vector<int>(3,0));
+            vector<vector<int>> curr(2,vector<int>(3,0));
+            
+            
+            
             for(int index=n-1;index>=0;index--)
             {
                     for(int buy=0;buy<=1;buy++)
@@ -30,15 +36,16 @@ public:
                             {
                                         if(buy)
                                         {
-                                                dp[index][buy][cap]=max((-prices[index]+dp[index+1][0][cap]),dp[index+1][1][cap]);
+                                                curr[buy][cap]=max((-prices[index]+ahead[0][cap]),ahead[1][cap]);
                                         }
                                         else
                                         {
-                                                dp[index][buy][cap]=max((prices[index]+dp[index+1][1][cap+1]),dp[index+1][0][cap]); 
+                                                curr[buy][cap]=max((prices[index]+ahead[1][cap+1]),ahead[0][cap]); 
                                         }
                             }
+                            ahead=curr;
                     }
             }
-            return dp[0][1][0];
+            return curr[1][0];
          }
 };
