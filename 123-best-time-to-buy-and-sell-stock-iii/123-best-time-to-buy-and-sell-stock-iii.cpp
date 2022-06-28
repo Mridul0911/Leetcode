85d166aa310e8465552cfda7d2ea073215b9157f
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2022-06-28 12:25:29 +0530

    Time: 951 ms (22.65%), Space: 207.1 MB (22.77%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
         int find(int index,int buy,int n,vector<int> &prices,int cap,vector<vector<vector<int>>> &dp)
         {
-                if(index==n || cap==0)
+                if(index==n || cap==2)
                 {
                         return 0;
                 }
@@ -14,13 +14,31 @@ public:
                 {
                         return dp[index][buy][cap]=max((-prices[index]+find(index+1,0,n,prices,cap,dp)),find(index+1,1,n,prices,cap,dp));
                 }
-                return dp[index][buy][cap]=max((prices[index]+find(index+1,1,n,prices,cap-1,dp)),find(index+1,0,n,prices,cap,dp));
+                return dp[index][buy][cap]=max((prices[index]+find(index+1,1,n,prices,cap+1,dp)),find(index+1,0,n,prices,cap,dp));
                 
         }
-    int maxProfit(vector<int>& prices) {
-        int n=prices.size();
+    int maxProfit(vector<int>& prices) 
+    {
+            int n=prices.size();
             int cap=0;
-            vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
-           return find(0,1,n,prices,2,dp);
-    }
+            vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+            for(int index=n-1;index>=0;index--)
+            {
+                    for(int buy=0;buy<=1;buy++)
+                    {
+                            for(int cap=1;cap>=0;cap--)
+                            {
+                                        if(buy)
+                                        {
+                                                dp[index][buy][cap]=max((-prices[index]+dp[index+1][0][cap]),dp[index+1][1][cap]);
+                                        }
+                                        else
+                                        {
+                                                dp[index][buy][cap]=max((prices[index]+dp[index+1][1][cap+1]),dp[index+1][0][cap]); 
+                                        }
+                            }
+                    }
+            }
+            return dp[0][1][0];
+         }
 };
