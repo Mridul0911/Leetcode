8dafd8e39566e09330475fe52bc3c5ad1dac31e0
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2022-06-30 16:14:19 +0530

    Time: 59 ms (71.50%), Space: 6.9 MB (87.28%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -40,6 +40,8 @@ bool find(string & S1, string & S2) {
 
 }
        bool isMatch(string s, string p) {
+                ios_base::sync_with_stdio(0);
+  cin.tie(0); cout.tie(0);
           return find(p,s);
        }
 };
