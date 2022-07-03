2c0ff13e13e93e51d2a7f44be3f43c40628d3c27
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2022-07-03 16:21:19 +0530

    Time: 3 ms (79.58%), Space: 11.1 MB (59.16%) - LeetHub

---
+++ 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+        bool isOperator(char gg)
+        {
+                if(gg=='*' || gg=='-' || gg=='+')
+                {
+                        return true;
+                }
+                else
+                {
+                        return false;
+                }
+        }
+        bool isOnlyNumber(string expression)
+        {
+                for(int i=0;i<expression.size();i++)
+                {
+                        if(expression[i]=='+' || expression[i]=='-' || expression[i]=='*')
+                        {
+                                return false;
+                        }
+                        
+                }
+                return true;
+        }
+        int findAns(int it,int it1,char operators)
+        {
+                if(operators=='*') return it*it1;
+                if(operators=='+') return it+it1;
+                if(operators=='-') return it-it1;
+                return 0;
+        }
+    vector<int> diffWaysToCompute(string expression) {
+        vector<int> ans;
+            int isNumber=0;
+            for(int i=0;i<expression.size();i++ )
+            {
+                    if(isOperator(expression[i]))
+                    {
+                        isNumber=1;
+                      vector<int> leftPart=diffWaysToCompute(expression.substr(0,i));
+                      vector<int> rightPart=diffWaysToCompute(expression.substr(i+1));
+                      for(auto it:leftPart)       
+                      {
+                              for(auto it1:rightPart)
+                              {
+                                      int ans1=findAns(it,it1,expression[i]);
+                                      ans.push_back(ans1);
+                              }
+                      }
+                     }
+            }
+            if(!isNumber)
+            {
+                    ans.push_back(stoi(expression));
+            }
+            return ans;
+    }
+};
