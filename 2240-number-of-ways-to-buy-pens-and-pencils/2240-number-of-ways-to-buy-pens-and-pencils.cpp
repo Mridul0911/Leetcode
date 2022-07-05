098c8afa1fe30d4bac589c5fd3bbc79d323d02ca
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2022-07-05 13:29:12 +0530

    Time: 64 ms (5.40%), Space: 37 MB (5.05%) - LeetHub

---
+++ 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long waysToBuyPensPencils(int total, int cost1, int cost2) {
+        if(total<0)
+        {
+                return 0;
+        }
+            else
+            {
+                    return 1+(total/cost2)+waysToBuyPensPencils(total-cost1,cost1,cost2);
+            }
+            
+    }
+};
