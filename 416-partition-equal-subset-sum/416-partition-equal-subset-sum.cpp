5dcb08ac320215b37782d34ba9543f174115ba43
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2022-07-06 14:40:48 +0530

    Time: 100 ms (88.67%), Space: 106.5 MB (9.54%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -18,15 +18,14 @@ public:
                    {
                       return  dp[n][sum]=isSubset(nums,n-1,sum,dp);
                    }
-        return  dp[n][sum]=(isSubset(nums,n-1,sum-nums[n-1],dp)||isSubset(nums,n-1,sum,dp));        
-        }
+return  dp[n][sum]=(isSubset(nums,n-1,sum-nums[n-1],dp)||isSubset(nums,n-1,sum,dp));         }
+    
     bool canPartition(vector<int>& nums) {
         int sum=0;
           for(int i=0;i<nums.size();i++)  
           {
                   sum+=nums[i];
           }
-          
             int n=nums.size();
            vector<vector<int>> dp(nums.size()+2,vector<int>((sum/2)+2,-1)); 
            if(sum%2!=0)
@@ -37,6 +36,5 @@ public:
             {
                     return isSubset(nums,nums.size(),sum/2,dp);
             }
-            
     }
 };
