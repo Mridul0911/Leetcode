2ffd56aba93944e6fff0e8b0a4b0cdca673936ec
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2022-07-06 14:45:10 +0530

    Time: 618 ms (12.37%), Space: 11.9 MB (60.04%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -27,14 +27,39 @@ return  dp[n][sum]=(isSubset(nums,n-1,sum-nums[n-1],dp)||isSubset(nums,n-1,sum,d
                   sum+=nums[i];
           }
             int n=nums.size();
-           vector<vector<int>> dp(nums.size()+2,vector<int>((sum/2)+2,-1)); 
            if(sum%2!=0)
            {
                    return false;
            }
             else
             {
-                    return isSubset(nums,nums.size(),sum/2,dp);
+                 //   return isSubset(nums,nums.size(),sum/2,dp);
+                vector<vector<bool>> dp(nums.size(),vector<bool>((sum/2)+1,false)); 
+                    for(int i=0;i<n;i++)
+                    {
+                            dp[i][0]=true;
+                    }
+                    if(nums[0]<=sum/2)
+                    {
+                            dp[0][nums[0]]=true;
+                    }
+                    for(int index=1;index<n;index++)
+                    {
+                            for(int target=1;target<=(sum/2);target++)
+                            {
+                                    bool nottake=dp[index-1][target];
+                                    bool take=false;
+                                    if(nums[index]<=target)
+                                    {
+                                            take=dp[index-1][target-nums[index]];
+                                    }
+                                    dp[index][target]=take || nottake;
+                            }
+                    }
+                    
+                    return dp[n-1][sum/2];
+                    
+                    
             }
     }
 };
