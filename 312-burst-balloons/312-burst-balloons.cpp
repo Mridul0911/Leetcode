daceb538226912bcf7676d81b6779dc875225c13
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2022-07-20 19:16:45 +0530

    Time: 600 ms (57.62%), Space: 10.4 MB (18.67%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.cpp
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
-        int dp[1001][1001];
-        int solve(vector<int>&nums,int i,int j)
+        int solve(vector<int> &nums,int i,int j,vector<vector<int>> &dp)
         {
-                if(i>=j) return 0;
-                if(dp[i][j]!=-1) return dp[i][j];
-                int c = INT_MIN;
-                for(int k=i;k<j;k++){
-                        int temp = solve(nums,i,k)+solve(nums,k+1,j)+(nums[i-1]*nums[k]*nums[j]);
-                        c = max(c,temp);
+                if(i>j)
+                {
+                        return 0;
                 }
-                return dp[i][j] = c;
+                if(dp[i][j]!=-1)
+                {
+                        return dp[i][j];
+                }
+                int maxi=INT_MIN;
+                for(int k=i;k<=j;k++)
+                {
+                        int cost=nums[i-1]*nums[j+1]*nums[k]+solve(nums,k+1,j,dp)+solve(nums,i,k-1,dp);
+                        maxi=max(maxi,cost);
+                }
+                return dp[i][j]=maxi;
         }
-        int maxCoins(vector<int>& nums){
+        int maxCoins(vector<int>& nums)
+        {
                 int n = nums.size();
-                memset(dp,-1,sizeof(dp));
                 nums.insert(nums.begin(),1);
                 nums.push_back(1);
-                return solve(nums,1,nums.size()-1);
+                cout<<nums[1]<<" "<<nums[n]<<endl;
+                vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+                return solve(nums,1,n,dp);
         }
 };        
