b8254e2a39aa1d7d16dd7cbba2b9953b763924d4
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2022-07-24 17:11:51 +0530

    Time: 1287 ms (27.23%), Space: 273.4 MB (52.25%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
@@ -31,6 +31,9 @@ public:
        //  }
        //  prev->next = NULL;
        //  return head;
+            ios_base::sync_with_stdio(0);
+cin.tie(0); cout.tie(0);
+
         ListNode* res = new ListNode(0);
         ListNode* ret = res;
         int sm = 0;
