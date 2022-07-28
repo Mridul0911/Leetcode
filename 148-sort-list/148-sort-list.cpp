625b093af6a2b3e1a028a8e7eafd248d09702fe1
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2022-07-28 15:07:32 +0530

    Time: 378 ms (26.30%), Space: 74.1 MB (29.52%) - LeetHub

--- 148-sort-list/148-sort-list.cpp
+++ 148-sort-list/148-sort-list.cpp
@@ -10,24 +10,70 @@
  */
 class Solution {
 public:
+        ListNode *findMid(ListNode *head)
+        {
+                ListNode *slow=head;
+                ListNode *fast=head->next;
+                while(fast!=NULL && fast->next!=NULL)
+                {
+                        slow=slow->next;
+                        fast=fast->next->next;
+                }
+                return slow;
+        }
+        ListNode *merge(ListNode *left,ListNode *right)
+        {
+                if(left==NULL)
+                {
+                        return right;
+                }
+                if(right==NULL)
+                {
+                        return left;
+                }
+                ListNode *ans=new ListNode(-1);
+                ListNode *temp=ans;
+                while(left!=NULL && right!=NULL)
+                {
+                        if(left->val<right->val)
+                        {
+                                temp->next=left;
+                                temp=left;
+                                left=left->next;
+                        }
+                        else
+                        {
+                                temp->next=right;
+                                temp=right;
+                                right=right->next;
+                        }
+                }
+                while(left!=NULL)
+                {
+                        temp->next=left;
+                        temp=left;
+                        left=left->next;
+                }
+                while(right!=NULL)
+                {
+                        temp->next=right;
+                        temp=right;
+                        right=right->next;
+                }
+                return ans->next;
+        }
     ListNode* sortList(ListNode* head) {
-            ListNode *newNode=head;
-            
-        vector<int> ans;
-            while(newNode!=NULL)
-            {
-                    ans.push_back(newNode->val);
-                    newNode=newNode->next;
-            }
-            sort(ans.begin(),ans.end());
-            newNode=head;
-            int index=0;
-            while(newNode!=NULL)
-            {
-                    newNode->val=ans[index++];
-                    newNode=newNode->next;
-            }
-            return head;
-             
+         if(head==NULL || head->next==NULL) 
+         {
+                 return head;
+         }
+            ListNode *mid=findMid(head);
+            ListNode *left=head;
+            ListNode *right=mid->next;
+            mid->next=NULL;
+            left=sortList(left);
+            right=sortList(right);
+            ListNode *result=merge(left,right);
+            return result;
     }
 };
