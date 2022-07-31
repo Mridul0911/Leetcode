d2a4ed57e4f5ce5687507cc129373c08f515045e
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2022-07-31 21:24:32 +0530

    Time: 9 ms (33.54%), Space: 9.6 MB (51.62%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -4,14 +4,11 @@ public:
     {
            int row_num = matrix.size();
 	int col_num = matrix[0].size();
-	
 	int begin = 0, end = row_num * col_num - 1;
-	
 	while(begin <= end)
         {
 		int mid = (begin + end) / 2;
 		int mid_value = matrix[mid/col_num][mid%col_num];
-		
 		if( mid_value == target)
                 {
 			return true;
@@ -25,9 +22,6 @@ public:
 			end = mid-1;
 		}
 	}
-	
 	return false;
-        
-        
     }
 };
