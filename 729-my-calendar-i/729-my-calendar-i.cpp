00f4af096cf0e7890f60ceada6f075279dafdbc3
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2022-08-03 19:17:32 +0530

    Time: 205 ms (32.97%), Space: 37.6 MB (94.67%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,28 +1,19 @@
 class MyCalendar {
 public:
-    map<int,int>m;
+    vector<pair<int,int>> inp;
     MyCalendar() {
     }
-    bool book(int start, int end) {
-        m[start]++;
-        m[end]--;
-        int sum=0;
-        for(auto it:m)
-        {
-            sum+=it.second;
-            if(sum>1)
+    bool book(int start, int end){
+            bool check=true;
+            for(auto it:inp)
             {
-                m[start]--;
-                m[end]++;
-                return false;
+                    if((start>= it.second || it.first>=end))continue;
+                    check=false;
             }
-        }
-        return true;
+            if(check)
+            {
+                    inp.push_back({start,end});
+            }
+            return check;
     }
 };
-
-/**
- * Your MyCalendar object will be instantiated and called as such:
- * MyCalendar* obj = new MyCalendar();
- * bool param_1 = obj->book(start,end);
- */
