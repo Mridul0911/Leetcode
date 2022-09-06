79a6f5b35aa4ad1a7d66726911f5ea667465684f
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2022-09-06 11:48:19 +0530

    Time: 152 ms (48.50%), Space: 10.5 MB (91.84%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,59 +1,50 @@
-class Solution {
-public:
-//     int f(vector<int> &coins,int index,int target,vector<vector<int>> &dp)
-//     {
-//         if(index==0)
-//         {
-//             if(target%coins[index]==0)
-//             {
-//                 return target/coins[index];
-//             }
-//             else
-//             {
-//                 return 1e9;
-//             }
-//         }
-//         if(dp[index][target]!=-1)
-//         {
-//             return dp[index][target];
-//         }
-//         int notpick=f(coins,index-1,target,dp);
-//         int pick=INT_MAX;
-//         if(coins[index]<=target)
-//         {
-//             pick=1+f(coins,index,target-coins[index],dp);
-//         }
-//         return dp[index][target]=min(pick,notpick);
-            
-//     }
-     int coinChange(vector<int>& arr, int T) {
-      int n= arr.size();
-    
-    vector<vector<int>> dp(n,vector<int>(T+1,0));
-    
-    for(int i=0; i<=T; i++){
-        if(i%arr[0] == 0)  
-            dp[0][i] = i/arr[0];
-        else dp[0][i] = 1e9;
-    }
-    
-    for(int ind = 1; ind<n; ind++){
-        for(int target = 0; target<=T; target++){
-            
-            int notTake = 0 + dp[ind-1][target];
-            int take = 1e9;
-            if(arr[ind]<=target)
-                take = 1 + dp[ind][target - arr[ind]];
-                
-             dp[ind][target] = min(notTake, take);
-        }
-    }
-    
-    int ans = dp[n-1][T];
-    if(ans >=1e9) return -1;
-    return ans;
-        
-        
-        
-    }
+class Solution { 
+public: 
+    int coinChange(vector<int>& coins, int sum) { 
+    int n = coins.size(); 
+    int dp[n+1][sum+1]; 
+ 
+    for(int i = 0; i<=n; i++){ 
+        for(int j = 0; j<=sum; j++){ 
+            dp[i][0] = 0; 
+            dp[0][j] = INT_MAX-1; //when size of array is 0 (so it is never possible to get that sum) 
+        } 
+    } 
+    dp[0][0] = INT_MAX-1; 
+    //fill first row 
+        for(int j = 1; j<=sum; j++){ 
+            if(j%coins[0] == 0){ 
+                dp[1][j] = j/coins[0]; 
+            } 
+            else{ 
+                dp[1][j] = INT_MAX-1; 
+            } 
+        } 
+     
+     for(int i = 2; i<=n; i++){ 
+        for(int j = 1; j<=sum; j++){ 
+            if(coins[i-1] <= j){ 
+            dp[i][j] = min(dp[i-1][j], 1+dp[i][j-coins[i-1]]); 
+            //take min of both when you didn't take the coin and when you take the coin(when you choose it then add 1 to increase coin count) 
+            } 
+            else{ 
+                dp[i][j] = dp[i-1][j]; 
+            } 
+        } 
+    } 
+            // for(int i=0;i<=n;i++)
+            // {
+            //         for(int j=0;j<=sum;j++)
+            //         {
+            //                 cout<<dp[i][j]<<" ";
+            //         }
+            //         cout<<endl;
+            // }
+            if(dp[n][sum]==2147483646)
+            {
+                    return -1;
+            }
+    return dp[n][sum]; 
+         
+    } 
 };
