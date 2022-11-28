d081b7e7c4d52705251bea68f1a9b6feaa871af6
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2022-11-28 17:32:57 +0530

    Time: 1336 ms (52.48%), Space: 178.5 MB (53.22%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -3,29 +3,24 @@ public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
             ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-    
-        map<int,pair<int,int>> mp; //wins ,losses
+      map<int,pair<int,int>> mp; //wins ,losses
             for(auto it:matches)
             {
                     mp[it[0]].first++;
                     mp[it[1]].second++;
             }
-            vector<int> ans1;
-            vector<int> ans2;
+            vector<vector<int>> answer(2);
             for(auto it:mp)
             {
                     if(it.second.second==0)
                     {
-                            ans1.push_back(it.first);
+                            answer[0].push_back(it.first);
                     }
                     else if(it.second.second==1)
                     {
-                            ans2.push_back(it.first);
+                            answer[1].push_back(it.first);
                     }
             }
-            vector<vector<int>> finalans;
-            finalans.push_back(ans1);
-            finalans.push_back(ans2);
-            return finalans;
+            return answer;
    }
 };
