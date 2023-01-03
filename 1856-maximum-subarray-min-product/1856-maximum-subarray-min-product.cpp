cb4a81720fc3efd7b2634a5b0e17a92a0a61a704
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2023-01-03 18:04:57 +0530

    Time: 619 ms (30.12%), Space: 90.2 MB (59.07%) - LeetHub

--- 1856-maximum-subarray-min-product/1856-maximum-subarray-min-product.cpp
+++ 1856-maximum-subarray-min-product/1856-maximum-subarray-min-product.cpp
@@ -6,14 +6,12 @@ public:
         int n=nums.size();
         vector<ll> prefix(n);
         prefix[0]=nums[0];
-        //calculate prefixSum
         for(int i=1;i<n;i++){
             prefix[i]=prefix[i-1]+ nums[i];
         }
          vector<int> left(nums.size());
             vector<int> right(nums.size());
             stack<int> st;
-            
            for(int i=0;i<nums.size();i++) 
            {
                while(!st.empty() && nums[st.top()]>=nums[i]) st.pop();
@@ -38,17 +36,12 @@ public:
                st.push(i);
            }
         ll ans=INT_MIN; 
-        //calculate max min-product
         for(int i=0;i<n;i++)
         {
-        // as we have stored the index -1 ->if no prev smallest,
-        // and index n ->if no next smallest.    
-            int lefti=left[i]+1; //left index of subarray 
-            int righti=right[i]-1; //right index of subarray 
-            //find subarray sum using prefix sum
-            ll subArraySum= lefti==0 ? prefix[righti] : (prefix[righti]-prefix[lefti-1]);
-            //take the max min-product everytime
-            ans=max(ans, nums[i]*subArraySum);
+            int lefti=left[i]+1;
+            int righti=right[i]-1;
+            ll subArraySum=lefti==0?prefix[righti]:(prefix[righti]-prefix[lefti-1]);
+            ans=max(ans,nums[i]*subArraySum);
         }
         return ans%M;
     }
