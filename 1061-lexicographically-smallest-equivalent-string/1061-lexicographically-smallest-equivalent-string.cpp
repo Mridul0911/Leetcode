740dd3e4c51fbf8ac2795c64a4e52ebc82c0a17a
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2023-01-14 18:34:48 +0530

    Time: 6 ms (49.54%), Space: 6.6 MB (60.55%) - LeetHub

---
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+  int find(int x,vector<int> &parent)
+  {
+     if(parent[x]==x)
+     {
+             return x;
+     }
+     else
+     {
+             return find(parent[x],parent);
+     }
+  }
+  string smallestEquivalentString(string s1, string s2, string baseStr) {
+        vector<int> parent(26,0);
+            int n=s1.size();
+            for(int i=0;i<26;i++)
+            {
+                    parent[i]=i;
+            }
+            for(int i=0;i<n;i++)
+            {
+                    int x=find(s1[i]-'a',parent);
+                    int y=find(s2[i]-'a',parent);
+                    if(x!=y)
+                    {
+                        parent[max(x,y)]=min(x,y);
+                    }
+            }
+            for(int i=0;i<baseStr.length();i++)
+            {
+                    baseStr[i]=find(baseStr[i]-'a',parent)+'a';
+            }
+            return baseStr;
+    }
+};
