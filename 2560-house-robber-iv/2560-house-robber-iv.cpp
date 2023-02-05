ccd5a08406f857b34a9683cf9eb656d8c3a61728
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2023-02-05 15:14:57 +0530

    Time: 184 ms (100.00%), Space: 56.9 MB (100.00%) - LeetHub

--- 2560-house-robber-iv/2560-house-robber-iv.cpp
+++ 2560-house-robber-iv/2560-house-robber-iv.cpp
@@ -1,44 +1,40 @@
 class Solution {
 public:
-        int find(int ind,vector<int> &nums,vector<int> &dp,int mid)
-        {
-                if(ind>=nums.size())
-                {
-                        return 0;
-                }
-                if(dp[ind]!=-1)
-                {
-                        return dp[ind];
-                }
-                int take=0;
-                if(nums[ind]<=mid)
-                {
-                        take=1+find(ind+2,nums,dp,mid);
-                }
-                int nottake=find(ind+1,nums,dp,mid);
-                return dp[ind]=max(take,nottake);
-        }
+        // int find(int ind,vector<int> &nums,vector<int> &dp,int mid)
+        // {
+        //         if(ind>=nums.size())
+        //         {
+        //                 return 0;
+        //         }
+        //         if(dp[ind]!=-1)
+        //         {
+        //                 return dp[ind];
+        //         }
+        //         int take=0;
+        //         if(nums[ind]<=mid)
+        //         {
+        //                 take=1+find(ind+2,nums,dp,mid);
+        //         }
+        //         int nottake=find(ind+1,nums,dp,mid);
+        //         return dp[ind]=max(take,nottake);
+        // }
     int minCapability(vector<int>& nums, int k) {
-       int n=nums.size();
-            int low=0;
-            int high=*max_element(nums.begin(),nums.end());
-            int finalans=0;
-            while(low<=high)
-            {
-                    int mid=(low+high)/2;
-                    vector<int> dp(n+1,-1);
-                    int ans=find(0,nums,dp,mid);
-                    // cout<<mid<<" "<<ans<<endl;
-                    if(ans>=k)       
-                    {
-                           high=mid-1;
-                           finalans=mid;
-                    }
-                    else
-                    {
-                            low=mid+1;
-                    }
-            } 
-            return finalans;
+       int left=1;
+          int right=1e9;
+          int n=nums.size();
+        while(left<right){
+            int mid=(left+right)/2;
+                int take = 0;
+            for(int i=0;i<n;i++)
+                if(nums[i]<=mid) {
+                    take+=1;
+                    i++;
+                }
+            if(take>=k)
+                right=mid;
+            else
+                left=mid+1;
+        }
+        return left;
     }
 };
