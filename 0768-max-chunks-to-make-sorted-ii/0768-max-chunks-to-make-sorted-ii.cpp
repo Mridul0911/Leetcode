0e770cceeccbfc0c5416b01b092557379db8c119
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2023-03-16 13:28:00 +0530

    Time: 29 ms (17.35%), Space: 16.4 MB (5.10%) - LeetHub

--- 0768-max-chunks-to-make-sorted-ii/0768-max-chunks-to-make-sorted-ii.cpp
+++ 0768-max-chunks-to-make-sorted-ii/0768-max-chunks-to-make-sorted-ii.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
-            vector<int> temp=arr;
+            int ans=0;
+          vector<int> temp=arr;
             sort(temp.begin(),temp.end());
-         map<int,vector<int>> mp;
-            for(int i=0;i<temp.size();i++)
+            map<int,vector<int>> mp;
+            for(int i=0;i<arr.size();i++)
             {
                     mp[temp[i]].push_back(i);
             }
-            int n=arr.size();
-            int ans=0;
             int i=arr.size()-1;
             while(i>=0)
             {
@@ -20,21 +19,21 @@ public:
                     }
                     else
                     {
-                          ans++;
-                          int pos=mp[arr[i]].back();
-                          mp[arr[i]].pop_back();
-                          int prev=arr[i];
-                          i--;
-                          while(i>=0 && i>=pos)
-                          {
-                              if(arr[i]<=prev)        
-                              {
-                                      prev=arr[i];
-                                      pos=mp[arr[i]].back();
-                                      mp[arr[i]].pop_back();
-                              }
-                              i--;
-                          }
+                            ans++;
+                            int pos=mp[arr[i]].back();
+                            mp[arr[i]].pop_back();
+                            int prev=arr[i];
+                            i--;
+                            while(i>=0 && i>=pos)
+                            {
+                                    if(arr[i]<=prev)
+                                    {
+                                            prev=arr[i];
+                                            pos=mp[arr[i]].back();
+                                            mp[arr[i]].pop_back();
+                                    }
+                                    i--;
+                            }
                     }
             }
             return ans;
