2381ba1dbdee7cb807e2cca06a2baaa8b83fe623
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2023-03-25 18:40:44 +0530

    Time: 147 ms (55.24%), Space: 111.8 MB (19.63%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
-          vector<int>  count1(7);
+         vector<int> count1(7);
             vector<int> count2(7);
             vector<int> same(7);
-            int n=tops.size();
             for(int i=0;i<tops.size();i++)
             {
                     count1[tops[i]]++;
@@ -14,21 +13,18 @@ public:
                             same[tops[i]]++;
                     }
             }
+            int n=tops.size();
             int ans=INT_MAX;
+            
             for(int i=1;i<=6;i++)
             {
-                    if(count1[i]+count2[i]-same[i]==n)
-                    {
-                            ans=min(ans,min(count1[i],count2[i])-same[i]);
+                    if(count1[i]+count2[i]-same[i]==n){
+                           ans=min(ans,min(count1[i],count2[i])-same[i]);
                     }
             }
-            if(ans==INT_MAX)
-            {
+            if(ans==INT_MAX){
                     return -1;
             }
-            else
-            {
-                    return ans;
-            }
+            return ans;
     }
 };
