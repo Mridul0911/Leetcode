7f7854f2ea32aff9357894f0678127d09099be3c
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2023-04-19 22:51:00 +0530

    Time: 168 ms (71.78%), Space: 94.1 MB (85.89%) - LeetHub

---
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+        int ans;
+        void solve(TreeNode *root,int score,int dir)
+        {
+                if(root==NULL)
+                {
+                        return;
+                }
+                ans=max(ans,score);
+                if(dir==0)
+                {
+                        solve(root->right,score+1,1);
+                        solve(root->left,1,0);
+                }
+                else
+                {
+                        solve(root->left, score + 1, 0); 
+                        solve(root->right, 1, 1);
+                }
+        }
+    int longestZigZag(TreeNode* root) {
+        solve(root->left,1,0);
+            solve(root->right,1,1);
+            return ans;
+    }
+};
