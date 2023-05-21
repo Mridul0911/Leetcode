2fe80374d2bb456a4d39804ba76acf0b07208e12
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2023-05-21 13:36:51 +0530

    Time: 1649 ms (5.29%), Space: 108 MB (11.26%) - LeetHub

--- 1452-people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list/1452-people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list.cpp
+++ 1452-people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list/1452-people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list.cpp
@@ -11,24 +11,66 @@ public:
         }
                return false;
     }
-    vector<int> peopleIndexes(vector<vector<string>>& inp) {
-        vector<set<string>> st;
-        vector<int> ans;
-        for(int i=0;i<inp.size();i++)
+    vector<int> peopleIndexes(vector<vector<string>>& a) {
+        // vector<set<string>> st;
+        // vector<int> ans;
+        // for(int i=0;i<inp.size();i++)
+        // {
+        //     set<string> temp;
+        //     for(int j=0;j<inp[i].size();j++)
+        //     {
+        //         temp.insert(inp[i][j]);
+        //     }
+        //     st.push_back(temp);
+        // }
+        // for(int i=0;i<st.size();i++)
+        // {
+        //     if(!isPossi(st,i)){
+        //         ans.push_back(i);
+        //     }
+        // }
+        //  return ans;  
+        unordered_map<int,unordered_map<string,bool>> m;
+        for(int i=0;i<a.size();i++)
         {
-            set<string> temp;
-            for(int j=0;j<inp[i].size();j++)
+            for(auto& it:a[i])
             {
-                temp.insert(inp[i][j]);
+                m[i][it]=true;
             }
-            st.push_back(temp);
         }
-        for(int i=0;i<st.size();i++)
+        vector<int> ans;
+        for(int i=0;i<a.size();i++)
         {
-            if(!isPossi(st,i)){
+            int j;
+            for(j=0;j<a.size();j++)
+            {
+                if(j==i)
+                {
+                    continue;
+                }
+                if(a[i].size()>a[j].size())
+                {
+                    continue;
+                }
+                int k;
+                for(k=0;k<a[i].size();k++)
+                {
+                    // cout<<a[i][k]<<endl;
+                    if(m[j][a[i][k]]==false)
+                    {
+                        break;
+                    }
+                }
+                if(k==a[i].size())
+                {
+                    break;
+                }
+            }
+            if(j==a.size())
+            {
                 ans.push_back(i);
             }
         }
-         return ans;  
+        return ans;
     }
 };
