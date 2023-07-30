c81d518a9f04f4a41b8e73532c1d16982088191b
Mridul jain <76435985+Mridul0911@users.noreply.github.com>
2023-07-30 16:06:05 +0530

    Time: 2 ms (64.08%), Space: 7.2 MB (28.55%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
+        int x=__gcd(str1.size(),str2.size());
         if(str1+str2==str2+str1)
         {
-                return str1.substr(0,__gcd(size(str1),size(str2)));
+        if(str1.size()>str2.size())
+        {
+            return str1.substr(0,x);
         }
         else
         {
-                return "";
+            return str2.substr(0,x);
+        }
+        }
+        else
+        {
+            return "";
         }
     }
 };
